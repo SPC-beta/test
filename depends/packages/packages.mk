87cd728204bb9f437d4b98bf43bbb11e7b15bf95
SPC-beta <93263160+SPC-beta@users.noreply.github.com>
2023-12-02 13:17:07 +0200

    Update packages.mk

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -1,23 +1,23 @@
 packages:=boost openssl libevent gmp zlib backtrace bls-dash
-darwin_packages:=zeromq
-linux_packages:=zeromq
-native_packages := native_ccache native_comparisontool native_cmake
 
-qt_native_packages = native_protobuf
 qt_packages = qrencode protobuf zlib
 
-qt_x86_64_linux_packages:=qt expat dbus libxcb xcb_proto libXau xproto freetype fontconfig libX11 xextproto libXext xtrans
-qt_i686_linux_packages:=$(qt_x86_64_linux_packages)
+qrencode_packages = qrencode
+
+qt_linux_packages:=qt expat libxcb xcb_proto libXau xproto freetype fontconfig
+qt_android_packages=qt
 
 qt_darwin_packages=qt
 qt_mingw32_packages=qt
 
-wallet_packages=bdb
+bdb_packages=bdb
+
+zmq_packages=zeromq
 
 upnp_packages=miniupnpc
 
 darwin_native_packages = native_biplist native_ds_store native_mac_alias
 
 ifneq ($(build_os),darwin)
-darwin_native_packages += native_cctools native_cdrkit native_libdmg-hfsplus
+darwin_native_packages += native_cctools native_libdmg-hfsplus
 endif
