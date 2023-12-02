7db4073a604eeec6fd076d12e866e87919198f97
SPC-beta <93263160+SPC-beta@users.noreply.github.com>
2023-12-02 15:34:21 +0200

    bls

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -1,7 +1,7 @@
-packages:=boost openssl libevent gmp zlib backtrace bls-dash
+packages:=boost openssl libevent gmp zlib backtrace cmake bls-dash
 darwin_packages:=zeromq
 linux_packages:=zeromq
-native_packages := native_ccache native_comparisontool native_cmake
+native_packages := native_ccache native_comparisontool
 
 qt_native_packages = native_protobuf
 qt_packages = qrencode protobuf zlib
