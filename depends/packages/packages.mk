ec548ea4328f2fbe5ee57c1b0050402293c33fff
SPC-beta <93263160+SPC-beta@users.noreply.github.com>
2023-12-03 21:08:06 +0200

    win

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -1,11 +1,12 @@
-packages:=boost openssl libevent gmp zlib backtrace cmake bls-dash
+packages:=boost openssl libevent gmp zlib backtrace bls-dash
 darwin_packages:=zeromq
 linux_packages:=zeromq
 native_packages := native_ccache native_comparisontool native_cmake
 
-qt_packages = qrencode zlib
+qt_native_packages = native_protobuf
+qt_packages = qrencode protobuf zlib
 
-qt_x86_64_linux_packages:=qt expat libxcb xcb_proto libXau xproto freetype fontconfig
+qt_x86_64_linux_packages:=qt expat dbus libxcb xcb_proto libXau xproto freetype fontconfig libX11 xextproto libXext xtrans
 qt_i686_linux_packages:=$(qt_x86_64_linux_packages)
 
 qt_darwin_packages=qt
