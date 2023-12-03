f3c8f3a8e25f5e8fac6596bb673f84d76e27f9a2
SPC-beta <93263160+SPC-beta@users.noreply.github.com>
2023-12-03 17:35:23 +0200

    qt1

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -3,9 +3,9 @@ darwin_packages:=zeromq
 linux_packages:=zeromq
 native_packages := native_ccache native_comparisontool
 
-qt_packages = qrencode protobuf zlib
+qt_packages = qrencode zlib
 
-qt_x86_64_linux_packages:=qt expat dbus libxcb xcb_proto libXau xproto freetype fontconfig libX11 xextproto libXext xtrans
+qt_x86_64_linux_packages:=qt expat libxcb xcb_proto libXau xproto freetype fontconfig
 qt_i686_linux_packages:=$(qt_x86_64_linux_packages)
 
 qt_darwin_packages=qt
