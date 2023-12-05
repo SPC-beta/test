a4a59369843883f56349f09ee84641fd18055a8b
SPC-beta <93263160+SPC-beta@users.noreply.github.com>
2023-12-05 18:22:26 +0200

    Update bitcoingui.cpp

--- src/qt/bitcoingui.cpp
+++ src/qt/bitcoingui.cpp
@@ -1512,6 +1512,7 @@ void BitcoinGUI::checkLelantusVisibility(int numBlocks)
 /** Get restart command-line parameters and request restart */
 void BitcoinGUI::handleRestart(QStringList args)
 {
+    LogPrintf ("Restarted\n");
     if (!ShutdownRequested())
         Q_EMIT requestedRestart(args);
 }
