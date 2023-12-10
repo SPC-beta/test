b7d3082107508cff4d6f3e1f8c6657445ba3e7f3
SPC-beta <93263160+SPC-beta@users.noreply.github.com>
2023-12-10 23:42:50 +0200

    Update packages.mk

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -3,7 +3,6 @@ darwin_packages:=zeromq
 linux_packages:=zeromq
 
 native_packages := native_ccache native_comparisontool native_cmake
-qt_native_packages = native_protobuf
 
 qt_native_packages = native_protobuf
 qt_packages = qrencode protobuf zlib
