0e05ee20bcb701e56ec966ea547485b72add8cd0
SPC-beta <93263160+SPC-beta@users.noreply.github.com>
2023-12-10 23:40:17 +0200

    Update packages.mk

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -2,6 +2,7 @@ packages:=boost openssl libevent gmp backtrace bls-dash
 darwin_packages:=zeromq
 linux_packages:=zeromq
 native_packages := native_ccache native_comparisontool native_cmake
+qt_native_packages = native_protobuf
 
 qt_packages = qrencode zlib protobuf
 
