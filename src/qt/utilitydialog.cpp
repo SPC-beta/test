730577ae88a1de9e8d6ac09e830a8bb7c40230b8
SPC-beta <93263160+SPC-beta@users.noreply.github.com>
2023-12-12 20:17:09 +0200

    Update utilitydialog.cpp

--- src/qt/utilitydialog.cpp
+++ src/qt/utilitydialog.cpp
@@ -40,14 +40,6 @@ HelpMessageDialog::HelpMessageDialog(QWidget *parent, bool about) :
     ui->setupUi(this);
 
     QString version = tr(PACKAGE_NAME) + " " + tr("version") + " " + QString::fromStdString(FormatFullVersion());
-    /* On x86 add a bit specifier to the version so that users can distinguish between
-     * 32 and 64 bit builds. On other architectures, 32/64 bit may be more ambiguous.
-     */
-#if defined(__x86_64__)
-    version += " " + tr("(%1-bit)").arg(64);
-#elif defined(__i386__ )
-    version += " " + tr("(%1-bit)").arg(32);
-#endif
 
     if (about)
     {  LogPrintf ("aboutClicked3.5\n");
