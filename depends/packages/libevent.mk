3f4035347d14391266ea3065c3ffca409cb3892c
SPC-beta <93263160+SPC-beta@users.noreply.github.com>
2023-12-26 22:47:32 +0200

    Update libevent.mk

--- depends/packages/libevent.mk
+++ depends/packages/libevent.mk
@@ -1,3 +1,4 @@
+package=libevent
 $(package)_version=2.1.8-stable
 $(package)_download_path=https://github.com/libevent/libevent/archive/
 $(package)_file_name=release-$($(package)_version).tar.gz
@@ -7,22 +8,10 @@ define $(package)_preprocess_cmds
   ./autogen.sh
 endef
 
-# When building for Windows, we set _WIN32_WINNT to target the same Windows
-# version as we do in configure. Due to quirks in libevents build system, this
-# is also required to enable support for ipv6. See #19375.
 define $(package)_set_vars
   $(package)_config_opts=--disable-shared --disable-openssl --disable-libevent-regress --disable-samples
-  $(package)_config_opts += --disable-dependency-tracking --enable-option-checking
   $(package)_config_opts_release=--disable-debug-mode
   $(package)_config_opts_linux=--with-pic
-  $(package)_config_opts_freebsd=--with-pic
-  $(package)_config_opts_netbsd=--with-pic
-  $(package)_config_opts_openbsd=--with-pic
-  $(package)_config_opts_android=--with-pic
-endef
-
-define $(package)_preprocess_cmds
-  cp -f $(BASEDIR)/config.guess $(BASEDIR)/config.sub build-aux
 endef
 
 define $(package)_config_cmds
@@ -38,7 +27,4 @@ define $(package)_stage_cmds
 endef
 
 define $(package)_postprocess_cmds
-  rm lib/*.la && \
-  rm include/ev*.h && \
-  rm include/event2/*_compat.h
 endef
