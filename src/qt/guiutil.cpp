19b650ba0fe4b5ca370c3698da9f8df50b54e0a5
SPC-beta <93263160+SPC-beta@users.noreply.github.com>
2023-12-28 02:29:25 +0200

    Update guiutil.cpp

--- src/qt/guiutil.cpp
+++ src/qt/guiutil.cpp
@@ -912,6 +912,9 @@ QString formatServicesStr(quint64 mask)
             case NODE_GETUTXO:
                 strList.append("GETUTXO");
                 break;
+            case NODE_BLOOM:
+                strList.append("BLOOM");
+                break;
             case NODE_WITNESS:
                 strList.append("WITNESS");
                 break;
