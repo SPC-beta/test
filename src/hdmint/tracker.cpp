0c9414977061ccfcfaeef1335a31f6d71557dc3c
SPC-beta <93263160+SPC-beta@users.noreply.github.com>
2024-11-22 16:08:19 +0200

    Update tracker.cpp

--- src/hdmint/tracker.cpp
+++ src/hdmint/tracker.cpp
@@ -215,7 +215,7 @@ std::vector<uint256> CHDMintTracker::GetSerialHashes()
  * @param hashPubcoin mint pubcoin hash
  * @return success
  */
-bool CHDMintTracker::HasPubcoinHash(const uint256& hashPubcoin, CWalletDB& walletdb) const
+bool CHDMintTracker::HasSigmaPubcoinHash(const uint256& hashPubcoin, CWalletDB& walletdb) const
 {
     for (auto const & it : mapSerialHashes) {
         CMintMeta meta = it.second;
@@ -223,6 +223,11 @@ bool CHDMintTracker::HasPubcoinHash(const uint256& hashPubcoin, CWalletDB& walle
             return true;
     }
 
+    return false;
+}
+
+bool CHDMintTracker::HasLelantusPubcoinHash(const uint256& hashPubcoin, CWalletDB& walletdb) const
+{
     for (auto const & it : mapLelantusSerialHashes) {
         CLelantusMintMeta meta = it.second;
         uint256 reducedHash;
@@ -546,7 +551,9 @@ bool CHDMintTracker::IsMempoolSpendOurs(const std::set<uint256>& setMempool, con
                 uint32_t pubcoinId;
                 try {
                     std::tie(spend, pubcoinId) = sigma::ParseSigmaSpend(txin);
-                } catch (...) {
+                } catch (CBadTxIn &) {
+                    return false;
+                } catch (std::ios_base::failure &) {
                     return false;
                 }
 
@@ -560,7 +567,9 @@ bool CHDMintTracker::IsMempoolSpendOurs(const std::set<uint256>& setMempool, con
                 std::unique_ptr<lelantus::JoinSplit> joinsplit;
                 try {
                     joinsplit = lelantus::ParseLelantusJoinSplit(tx);
-                } catch (...) {
+                } catch (CBadTxIn &) {
+                    return false;
+                } catch (std::ios_base::failure &) {
                     return false;
                 }
 
