f6be61571c8a7ef67d579527013dab797051dfa1
SPC-beta <93263160+SPC-beta@users.noreply.github.com>
2024-11-27 17:59:51 +0200

    x

--- src/clientversion.h
+++ src/clientversion.h
@@ -9,16 +9,6 @@
 #include "config/bitcoin-config.h"
 #else
 
-/**
- * client versioning and copyright year
- */
-
-//! These need to be macros, as clientversion.cpp's and bitcoin*-res.rc's voodoo requires it
-#define CLIENT_VERSION_MAJOR 5
-#define CLIENT_VERSION_MINOR 14
-#define CLIENT_VERSION_REVISION 12
-#define CLIENT_VERSION_BUILD 1
-
 //! Set to true for release, false for prerelease or test build
 #define CLIENT_VERSION_IS_RELEASE true
 
