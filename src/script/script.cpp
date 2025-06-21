4b9b5bc818ce908a9a88009183e657fd4fafc63c
unknown <bzxtests11@protonmail.com>
2025-06-22 02:17:43 +0300

    Update script.cpp

--- src/script/script.cpp
+++ src/script/script.cpp
@@ -342,7 +342,7 @@ bool CScript::IsSparkSpend() const {
 }
 
 bool CScript::IsMint() const {
-    return IsSigmaMint() || IsLelantusMint() || IsLelantusJMint() || IsSparkMint() || IsSparkSMint()
+    return IsSigmaMint() || IsLelantusMint() || IsLelantusJMint() || IsSparkMint() || IsSparkSMint();
 }
 
 bool CScript::HasCanonicalPushes() const
