b328a2bada3cebbfdf09464f8f691956bd154201
unknown <bzxtests11@protonmail.com>
2025-06-23 19:34:02 +0300

    Update optionsdialog.cpp

--- src/qt/optionsdialog.cpp
+++ src/qt/optionsdialog.cpp
@@ -84,7 +84,7 @@ OptionsDialog::OptionsDialog(QWidget *parent, bool enableWallet) :
 
     ui->lang->setToolTip(ui->lang->toolTip().arg(tr(PACKAGE_NAME)));
     ui->lang->addItem(QString("(") + tr("default") + QString(")"), QVariant(""));
-    Q_FOREACH(const QString &langStr, translations.entryList())
+    for (const QString &langStr : translations.entryList())
     {
         QLocale locale(langStr);
 
@@ -185,10 +185,13 @@ void OptionsDialog::setMapper()
     mapper->addMapping(ui->spendZeroConfChange, OptionsModel::SpendZeroConfChange);
     mapper->addMapping(ui->reindexLelantus, OptionsModel::ReindexLelantus);
     mapper->addMapping(ui->coinControlFeatures, OptionsModel::CoinControlFeatures);
-    mapper->addMapping(ui->enableRapAddresses, OptionsModel::enableRapAddresses);
 
     /* Lelantus */
     mapper->addMapping(ui->autoAnonymize, OptionsModel::AutoAnonymize);
+    mapper->addMapping(ui->fSplit, OptionsModel::Split);
+    if (!lelantus::IsLelantusAllowed()) {
+        ui->lelantusPage->setVisible(false);
+    }
     mapper->addMapping(ui->lelantusPage, OptionsModel::LelantusPage);
 
     /* Network */
