0292da9a8dba102f406ecf46be87f506d0e21e9a
unknown <bzxtests11@protonmail.com>
2025-06-24 18:12:36 +0300

    Revert "xx"
    This reverts commit d5b69753ccf7f17d05a447e0aae1c8361547e057.

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -1,26 +1,31 @@
-packages:=
-
-boost_packages = boost
-
-libevent_packages = libevent
-
-qrencode_linux_packages = qrencode
-qrencode_darwin_packages = qrencode
-qrencode_mingw32_packages = qrencode
+packages:=boost openssl libevent gmp zlib backtrace tor bls-dash
+darwin_packages:=zeromq
+linux_packages:=zeromq
+native_packages := native_ccache native_cmake
 
 qt_linux_packages:=qt expat libxcb xcb_proto libXau xproto freetype fontconfig libxkbcommon libxcb_util libxcb_util_render libxcb_util_keysyms libxcb_util_image libxcb_util_wm
+
 qt_darwin_packages=qt
 qt_mingw32_packages=qt
 
 bdb_packages=bdb
 sqlite_packages=sqlite
+qrencode_packages=qrencode
 
 zmq_packages=zeromq
 
 upnp_packages=miniupnpc
-natpmp_packages=libnatpmp
 
-multiprocess_packages = libmultiprocess capnp
-multiprocess_native_packages = native_libmultiprocess native_capnp
+darwin_native_packages = native_ds_store native_mac_alias
 
-usdt_linux_packages=systemtap
+$(host_arch)_$(host_os)_native_packages += native_b2
+
+ifneq ($(build_os),darwin)
+darwin_native_packages += native_cctools native_libtapi native_libdmg-hfsplus
+
+ifeq ($(strip $(FORCE_USE_SYSTEM_CLANG)),)
+darwin_native_packages+= native_clang
+endif
+
+endif
+
