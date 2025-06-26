f18359a037fed5a21d85d0f8f72d8523fe13d80c
unknown <bzxtests11@protonmail.com>
2025-06-26 13:06:40 +0300

    Update packages.mk

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -19,8 +19,6 @@ zmq_packages=zeromq
 upnp_packages=miniupnpc
 natpmp_packages=libnatpmp
 
-$(host_arch)_$(host_os)_native_packages += native_b2
-
 multiprocess_packages = libmultiprocess capnp
 multiprocess_native_packages = native_libmultiprocess native_capnp
 
