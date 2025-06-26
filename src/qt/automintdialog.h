399d647d8f5b294a97b2e146d3baade224d821a0
unknown <bzxtests11@protonmail.com>
2025-06-26 18:28:43 +0300

    x

---
+++ src/qt/automintdialog.h
@@ -0,0 +1,54 @@
+#ifndef BZX_QT_AUTOMINT_H
+#define BZX_QT_AUTOMINT_H
+
+#include "walletmodel.h"
+
+#include <QDialog>
+#include <QPainter>
+#include <QPaintEvent>
+
+namespace Ui {
+    class AutoMintDialog;
+}
+
+enum class AutoMintSparkMode : uint8_t {
+    MintAll, // come from overview page
+    AutoMintAll // come from notification
+};
+
+class AutoMintSparkDialog : public QDialog
+{
+    Q_OBJECT;
+
+public:
+    explicit AutoMintSparkDialog(AutoMintSparkMode mode, QWidget *parent = 0);
+    ~AutoMintSparkDialog();
+
+public:
+    int exec();
+    void setModel(WalletModel *model);
+
+    void paintEvent(QPaintEvent *event) Q_DECL_OVERRIDE;
+
+private Q_SLOTS:
+    void accept();
+    void reject();
+
+private:
+    enum class AutoMintSparkProgress : uint8_t {
+        Start,
+        Unlocking,
+        Minting
+    };
+
+    Ui::AutoMintDialog *ui;
+    WalletModel *model;
+    SparkModel *sparkModel;
+    bool requiredPassphase;
+    AutoMintSparkProgress progress;
+    AutoMintSparkMode mode;
+
+    void ensureSparkModel();
+};
+
+#endif // BZX_QT_AUTOMINT_H
