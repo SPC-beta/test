509b61c1944a952f2dec9015fc18e2705caff191
unknown <bzxtests11@protonmail.com>
2025-07-01 18:23:03 +0300

    x

--- src/mbstring.h
+++
@@ -1,9 +0,0 @@
-#ifndef BZX_MBSTRING_H
-#define BZX_MBSTRING_H
-
-#include <string>
-
-/** Replaces invalid UTF-8 characters or character sequences with question marks. */
-std::string SanitizeInvalidUTF8(const std::string& s);
-
-#endif // BZX_MBSTRING_H
