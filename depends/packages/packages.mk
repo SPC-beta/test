a32d60e4488e6b1a1e77f8fd3948c9c5247a0a23
unknown <bzxtests11@protonmail.com>
2025-07-01 17:57:54 +0300

    cm

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -1,5 +1,4 @@
-packages:= boost openssl libevent gmp zlib backtrace tor bls-dash
-native_packages := native_cmake
+packages:= boost openssl libevent gmp zlib backtrace cmake tor bls-dash
 qt_linux_packages:= qt expat libxcb xcb_proto libXau xproto freetype fontconfig libxkbcommon libxcb_util libxcb_util_render libxcb_util_keysyms libxcb_util_image libxcb_util_wm
 
 qrencode_linux_packages = qrencode
