3434ce9db6538f93d73739261c8c023951fc2b03
unknown <bzxtests11@protonmail.com>
2025-07-07 17:55:36 +0300

    xx

--- depends/hosts/openbsd.mk
+++ depends/hosts/openbsd.mk
@@ -1,11 +1,5 @@
-openbsd_CFLAGS=-pipe -std=$(C_STANDARD)
-openbsd_CXXFLAGS=-pipe -std=$(CXX_STANDARD)
-
-ifneq ($(LTO),)
-openbsd_CFLAGS += -flto
-openbsd_CXXFLAGS += -flto
-openbsd_LDFLAGS += -flto
-endif
+openbsd_CFLAGS=-pipe
+openbsd_CXXFLAGS=$(openbsd_CFLAGS)
 
 openbsd_release_CFLAGS=-O2
 openbsd_release_CXXFLAGS=$(openbsd_release_CFLAGS)
