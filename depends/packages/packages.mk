dfe83a74b93e5e6fc58e0f5e8b2ee5b979bc2008
SPC-beta <93263160+SPC-beta@users.noreply.github.com>
2025-09-22 18:46:03 +0300

    Update packages.mk

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -1,15 +1,21 @@
-packages:=boost openssl libevent gmp zlib backtrace tor bls-dash
+packages:=boost openssl libevent gmp zlib backtrace tor bls-dash qrencode 
 darwin_packages:=zeromq
 linux_packages:=zeromq
 native_packages :=
 
 qt_linux_packages:=qt expat libxcb xcb_proto libXau xproto freetype fontconfig libxkbcommon libxcb_util libxcb_util_render libxcb_util_keysyms libxcb_util_image libxcb_util_wm
 
+qrencode_linux_packages = qrencode
+qrencode_android_packages = qrencode
+qrencode_darwin_packages = qrencode
+qrencode_mingw32_packages = qrencode
+
+upnp_packages=miniupnpc
+
 qt_darwin_packages=qt
 qt_mingw32_packages=qt
 
 bdb_packages=bdb
-qrencode_packages=qrencode
 
 zmq_packages=zeromq
 
