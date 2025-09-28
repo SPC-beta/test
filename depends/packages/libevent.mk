65b0c40d9910cbf532061d7f48ebc516a3cfe4ec
SPC-beta <93263160+SPC-beta@users.noreply.github.com>
2025-09-29 00:04:01 +0300

    Update libevent.mk

--- depends/packages/libevent.mk
+++ depends/packages/libevent.mk
@@ -39,6 +39,8 @@ define $(package)_stage_cmds
 endef
 
 define $(package)_postprocess_cmds
-  rm lib/*.la && \
-  rm include/ev*.h
+  rm -rf bin lib/pkgconfig && \
+  rm include/ev*.h && \
+  rm include/event2/*_compat.h && \
+  rm lib/libevent.a
 endef
