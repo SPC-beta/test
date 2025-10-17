b28b2dd9f448e092e1ba581dbee5187bd50a34e7
SPC-beta <93263160+SPC-beta@users.noreply.github.com>
2025-10-17 21:35:09 +0300

    Update FindZeroMQ.cmake

--- cmake/module/FindZeroMQ.cmake
+++ cmake/module/FindZeroMQ.cmake
@@ -16,28 +16,26 @@ This is a wrapper around find_package()/pkg_check_modules() commands that:
 
 include(FindPackageHandleStandardArgs)
 find_package(ZeroMQ ${ZeroMQ_FIND_VERSION} NO_MODULE QUIET)
-if(NOT TARGET zeromq)
-  if(ZeroMQ_FOUND)
-    find_package_handle_standard_args(ZeroMQ
-      REQUIRED_VARS ZeroMQ_DIR
-      VERSION_VAR ZeroMQ_VERSION
-    )
-    if(TARGET libzmq)
-      add_library(zeromq ALIAS libzmq)
-    elseif(TARGET libzmq-static)
-      add_library(zeromq ALIAS libzmq-static)
-    endif()
-    mark_as_advanced(ZeroMQ_DIR)
-  else()
-    find_package(PkgConfig REQUIRED)
-    pkg_check_modules(libzmq QUIET
-      IMPORTED_TARGET
-      libzmq>=${ZeroMQ_FIND_VERSION}
-    )
-    find_package_handle_standard_args(ZeroMQ
-      REQUIRED_VARS libzmq_LIBRARY_DIRS
-      VERSION_VAR libzmq_VERSION
-    )
-    add_library(zeromq ALIAS PkgConfig::libzmq)
+if(ZeroMQ_FOUND)
+  find_package_handle_standard_args(ZeroMQ
+    REQUIRED_VARS ZeroMQ_DIR
+    VERSION_VAR ZeroMQ_VERSION
+  )
+  if(TARGET libzmq)
+    add_library(zeromq ALIAS libzmq)
+  elseif(TARGET libzmq-static)
+    add_library(zeromq ALIAS libzmq-static)
   endif()
+  mark_as_advanced(ZeroMQ_DIR)
+else()
+  find_package(PkgConfig REQUIRED)
+  pkg_check_modules(libzmq QUIET
+    IMPORTED_TARGET
+    libzmq>=${ZeroMQ_FIND_VERSION}
+  )
+  find_package_handle_standard_args(ZeroMQ
+    REQUIRED_VARS libzmq_LIBRARY_DIRS
+    VERSION_VAR libzmq_VERSION
+  )
+  add_library(zeromq ALIAS PkgConfig::libzmq)
 endif()
